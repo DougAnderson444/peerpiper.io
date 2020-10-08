2cda056a07bcd714254a70782756e8eaab8a11c7
DougA <douganderson444@gmail.com>
2020-10-07 21:32:04 -0400

    refactor to svelte

---
+++ src/App.svelte
@@ -0,0 +1,80 @@
+<script>
+  import { onMount } from "svelte";
+  export let date;
+  onMount(async () => {
+    const res = await fetch("/api/date");
+    const newDate = await res.text();
+    date = newDate;
+  });
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  .hero {
+    margin: 3rem 1rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    color: #525252;
+    width: 100%;
+  }
+  body > * {
+    margin: 3rem 1rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+    color: #525252;
+  }
+  h2 {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+  .description {
+    text-align: center;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+  .container {
+    display: flex;
+    justify-content: center;
+    padding: 1em;
+    margin: 1em;
+    border: 2px solid #C0C0C0;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+  }
+</style>
+
+<main>
+  <div class="hero">
+    <center>
+      <img
+        width="150"
+        height="150"
+        layout="fixed"
+        src="P.png"
+        alt="peerpiper.io logo" />
+    </center>
+    <h1 class="title">PeerPiper.io</h1>
+    <p class="description">
+      Your ultimate personal data sink. Save once, pipe out to your peers.
+    </p>
+    <h2>The date is:</h2>
+    <p>{date ? date : 'Loading date...'}</p>
+
+  </div>
+</main>
