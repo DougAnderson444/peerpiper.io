198421bc0851ee45cbef401b8d693e577fa43e6e
DougA <douganderson444@gmail.com>
2020-11-03 10:49:52 -0500

    beaer token from env

--- api/post.js
+++ api/post.js
@@ -3,13 +3,11 @@ const fetch = require("node-fetch"); // to make fetch work in nodejs
 
 module.exports = async (req, res) => {
 
-    console.log(` sp: ${process.env.SUPERPEER} `)
-
     const response = await fetch(process.env.SUPERPEER, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.TOKEN}`            
+            Authorization: `Bearer ${process.env.TOKEN}`
           },
         body: JSON.stringify(req.body), // body data type must match "Content-Type" header
     });
