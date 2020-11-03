d2ba04e8e78467b253bdec46c30245ba2bdf02aa
DougA <douganderson444@gmail.com>
2020-11-02 20:40:51 -0500

    trbleshooting vercel

--- api/post.js
+++ api/post.js
@@ -4,7 +4,8 @@ const fetch = require("node-fetch"); // to make fetch work in nodejs
 module.exports = async (req, res) => {
 
     console.log(` sp: ${process.env.SUPERPEER} `)
-    
+    try {
+        
     const response = await fetch(process.env.SUPERPEER, {
         method: "POST",
         headers: {
@@ -13,5 +14,16 @@ module.exports = async (req, res) => {
           },
         body: JSON.stringify(req.body), // body data type must match "Content-Type" header
     });
-    return await response.json(); // parses JSON response into native JavaScript objects
+    
+    console.log(` response raw: `, response)
+    const res = await response
+    console.log(` response: `, res)
+    const r = await response.json()
+    console.log(` response.json: `, r)
+    return r // await response.json(); // parses JSON response into native JavaScript objects
+
+    } catch (error) {
+        return {error}        
+    }
+
 }
