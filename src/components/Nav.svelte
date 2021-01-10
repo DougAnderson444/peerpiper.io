70f0e1b7ea0416d6fd5b53cb912749f5bc75f537
Doug <douganderson444@gmail.com>
2021-01-09 21:02:34 -0500

    Nav bar exclude subdomain

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,19 @@
 <script>
+	import { onMount } from "svelte";
+
 	export let segment;
+	let host = "localhost:3000";
+	onMount(async () => {
+		host = window.location.hostname;
+		host += window.location.hostname.includes("localhost") ? ":3000" : "";
+	});
 </script>
 
 <style>
+	header {
+		box-shadow: 0 -0.4rem 0.9rem 0.2rem rgba(0, 0, 0, 0.5);
+		margin-bottom: 2em;
+	}
 	nav {
 		border-bottom: 1px solid rgba(58, 179, 47, 0.1);
 		font-weight: 300;
@@ -46,28 +57,49 @@
 		padding: 1em 0.5em;
 		display: block;
 	}
+	.home {
+		position: relative;
+		top: 0.85rem;
+		left: 0.25rem;
+		width: 1.5rem;
+		height: 0rem;
+		-webkit-tap-highlight-color: transparent;
+		-webkit-touch-callout: none;
+		background: 0 50% no-repeat;
+		background-size: auto 100%;
+		text-indent: -9999px;
+	}
 </style>
 
-<nav>
-	<ul>
-		<li>
-			<a
-				aria-current={segment === undefined ? 'page' : undefined}
-				href=".">home</a>
-		</li>
-		<li>
-			<a
-				aria-current={segment === 'about' ? 'page' : undefined}
-				href="about">about</a>
-		</li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li>
-			<a
-				rel="prefetch"
-				aria-current={segment === 'blog' ? 'page' : undefined}
-				href="blog">blog</a>
-		</li>
-	</ul>
-</nav>
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a
+					rel="prefetch"
+					href="."
+					class="home"
+					title="Homepage"
+					style="background-image:url(p.svg)">Home</a>
+			</li>
+			<li>
+				<a
+					aria-current={segment === 'app' ? 'page' : undefined}
+					href="http://app.{host}">app</a>
+			</li>
+			<li>
+				<a
+					rel="prefetch"
+					aria-current={segment === 'rss' ? 'page' : undefined}
+					href="rss">blog</a>
+			</li>
+			<li>
+				<a
+					aria-current={segment === 'about' ? 'page' : undefined}
+					href="about">about</a>
+			</li>
+			<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+			 the blog data when we hover over the link or tap it on a touchscreen -->
+		</ul>
+	</nav>
+</header>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,25 +1,46 @@
 <script>
-	import Nav from "../components/Nav.svelte";
-	import HyPNSManager from "../components/HyPNSManager.svelte";
-	import IPFSManager from "../components/IPFSManager.svelte";
-	export let segment;
+  import { onMount } from "svelte";
+
+  import Nav from "../components/Nav.svelte";
+  import HyPNSManager from "../components/HyPNSManager.svelte";
+  import IPFSManager from "../components/IPFSManager.svelte";
+  export let segment;
+
+  let subdomain = false;
+
+  onMount(async () => {
+    let isDev = window.location.hostname.includes("localhost");
+    let splitHost = window.location.hostname.split(".");
+
+    if (
+      (!isDev && splitHost.length === 3) ||
+      (isDev && splitHost.length === 2)
+    ) {
+      subdomain = splitHost[0];
+    }
+  });
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 0 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment} />
+{#if !subdomain}
+  <Nav {segment} />
+{/if}
 
 <main>
-	<slot />
+  <slot />
 </main>
-<HyPNSManager />
-<IPFSManager />
+
+{#if subdomain}
+  <HyPNSManager />
+  <IPFSManager />
+{/if}
