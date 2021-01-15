0adc7ca069905ab16facf7d16f94e58c64b5965b
Doug <douganderson444@gmail.com>
2021-01-15 12:53:24 -0500

    fix: rss

--- src/routes/rss.svelte
+++ src/routes/rss.svelte
@@ -12,14 +12,45 @@
 
 <script>
     export let feed;
-    // import { onMount } from "svelte";
-    // onMount(async () => {
-    //     const res = await fetch("api/getRssFeed", {
-    //         method: "GET",
-    //     });
-    //     feed = await res.json();
-    // });
+    import { onMount } from "svelte";
+    onMount(async () => {
+        if (!feed) {
+            const res = await fetch("api/getRssFeed", {
+                method: "GET",
+            });
+            feed = await res.json();
+        }
+    });
 </script>
+
+<svelte:head>
+    <title>PeerPiper Blog</title>
+</svelte:head>
+
+<h1>Recent posts</h1>
+
+{#if feed}
+    <div class="container">
+        {#each feed.items.reverse() as item, i}
+            <a href={item.link} target="_blank">
+                <div class="card">
+                    <h2>{item.title}</h2>
+                    <h5>
+                        {new Date(item.pubDate).toLocaleDateString("en-US", {
+                            weekday: "long",
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                        })}
+                    </h5>
+                    <!-- <div class="" style="">
+                        {@html item['content:encoded']}
+                    </div> -->
+                </div>
+            </a>
+        {/each}
+    </div>
+{/if}
 
 <style>
     .container {
@@ -39,32 +70,3 @@
         text-decoration: none;
     }
 </style>
-
-<svelte:head>
-    <title>PeerPiper Blog</title>
-</svelte:head>
-
-<h1>Recent posts</h1>
-
-{#if feed}
-    <div class="container">
-        {#each feed.items.reverse() as item, i}
-            <a href={item.link} target="_blank">
-                <div class="card">
-                    <h2>{item.title}</h2>
-                    <h5>
-                        {new Date(item.pubDate).toLocaleDateString('en-US', {
-                            weekday: 'long',
-                            year: 'numeric',
-                            month: 'long',
-                            day: 'numeric',
-                        })}
-                    </h5>
-                    <!-- <div class="" style="">
-                        {@html item['content:encoded']}
-                    </div> -->
-                </div>
-            </a>
-        {/each}
-    </div>
-{/if}
