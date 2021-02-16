ec07bc84cc95d2c3c88b98c64f104bc7c94ddbee
Doug <douganderson444@gmail.com>
2021-02-16 09:12:48 -0500

    add: design notes

--- src/components/AppSections/PeerPiper.svelte
+++ src/components/AppSections/PeerPiper.svelte
@@ -2,6 +2,22 @@
   // stores
   import { myInstance } from "../../js/stores.js";
   import EditData from "../EditData.svelte";
+  // import DIDManager from "../DIDManager.svelte";
+
+  /**
+   * When a subdomain is first created, we need to 
+   * 1. Take the hypercore device seed and authorize it on the DID  
+      a. check to see if this subdomain is already claimed: database]
+      (if claimed:)
+      b. show from a guest node
+      (if not claimed:)
+      a. make Hypns for this subdomain
+      b. get seed
+      c. make device material
+      d. write it to DID
+      e. associate the DID with this subdomain (dnslink)
+      f. assume most users will spend time at their own subdomain since their contacts data is rolled up there anyway 
+   */
 </script>
 
 <svelte:head>
@@ -13,5 +29,7 @@
   Your PublicKey:
   {#if $myInstance && $myInstance.publicKey}{$myInstance.publicKey}{/if}
   <br />
+
   <EditData />
+  <!-- <DIDManager /> -->
 </div>
