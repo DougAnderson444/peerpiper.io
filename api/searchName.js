601dafc1be20788d9989ec9d41d015ecd9f404c2
Doug <douganderson444@gmail.com>
2021-03-05 11:36:10 -0500

    update vercel search fn

--- api/searchName.js
+++ api/searchName.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch') // to make fetch work in nodejs
 
 module.exports = async (req, res) => {
-  console.log('get searching...', req.query.search)
+  console.log('get searching...', `${process.env.HYPERBEE_SUPERPEER}${req.query.search}`)
   try {
     const response = await fetch(`${process.env.HYPERBEE_SUPERPEER}${req.query.search}`, {
       method: 'GET',
@@ -12,6 +12,7 @@ module.exports = async (req, res) => {
     })
 
     const r = await response.json() // parses JSON response into native JavaScript objects
+    console.log({ r })
     res.status(200).json(r)
   } catch (error) {
     console.error(error)
