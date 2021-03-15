1844572e7f16b27f01d3f8e53b07ac176e66070f
Doug <douganderson444@gmail.com>
2021-03-15 13:36:26 -0400

    feat: add email subscription

--- src/components/graphics/GetInvited.svelte
+++ src/components/graphics/GetInvited.svelte
@@ -51,6 +51,73 @@
                 </svg>
             </div>
         </a>
+        <div class="col">
+            <p>or</p>
+        </div>
+        <div class="col">
+            <!-- Begin Mailchimp Signup Form -->
+            <link
+                href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css"
+                rel="stylesheet"
+                type="text/css"
+            />
+            <style type="text/css">
+                #mc_embed_signup {
+                    background: #fff;
+                    clear: left;
+                    font: 14px Helvetica, Arial, sans-serif;
+                }
+                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+                <form
+                    action="https://PeerPiper.us4.list-manage.com/subscribe/post?u=68f88d6e8a4e581cb43d66661&amp;id=14ba97cdbf"
+                    method="post"
+                    id="mc-embedded-subscribe-form"
+                    name="mc-embedded-subscribe-form"
+                    class="validate"
+                    target="_blank"
+                    novalidate
+                >
+                    <div id="mc_embed_signup_scroll">
+                        <label for="mce-EMAIL">Subscribe</label>
+                        <input
+                            type="email"
+                            value=""
+                            name="EMAIL"
+                            class="email"
+                            id="mce-EMAIL"
+                            placeholder="email address"
+                            required
+                        />
+                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div
+                            style="position: absolute; left: -5000px;"
+                            aria-hidden="true"
+                        >
+                            <input
+                                type="text"
+                                name="b_68f88d6e8a4e581cb43d66661_14ba97cdbf"
+                                tabindex="-1"
+                                value=""
+                            />
+                        </div>
+                        <div class="clear">
+                            <input
+                                type="submit"
+                                value="Subscribe"
+                                name="subscribe"
+                                id="mc-embedded-subscribe"
+                                class="button"
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <!--End mc_embed_signup-->
+        </div>
     </div>
 {/if}
 
@@ -65,6 +132,7 @@
         display: flex;
         flex-direction: row;
         justify-content: center;
+        align-items: center;
     }
     .col {
         margin: 1rem;
